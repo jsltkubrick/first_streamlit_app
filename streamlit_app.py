8e8c40c0f513ab488e2669c28cf0aaf38f49da7f
jsltkubrick <127206945+jsltkubrick@users.noreply.github.com>
2023-04-05 21:45:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner') #sets the app's title - committing this change makes the title appear in the streamlit app
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, spinach and rocket smoothie')
+streamlit.text('hard boiled free-range egg')
