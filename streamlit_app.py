f1aea739df9f75156935345aeea833746327ebe0
jsltkubrick <127206945+jsltkubrick@users.noreply.github.com>
2023-04-05 21:57:04 +0100

    Update streamlit_app.py
    adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import streamlit
 streamlit.title('My Parents new healthy diner') #sets the app's title - committing this change makes the title appear in the streamlit app
 
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, spinach and rocket smoothie')
-streamlit.text('hard boiled free-range egg')
+streamlit.text(' 🥣Omega 3 & Blueberry oatmeal')
+streamlit.text(' 🥗 Kale, spinach and rocket smoothie')
+streamlit.text(' 🐔 hard boiled free-range egg')
+streamlit.text(' 🥑🍞avo toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
