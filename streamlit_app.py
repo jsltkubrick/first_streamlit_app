f20197b64875bfa4f57e81eeeff09f87e2527e62
jsltkubrick <127206945+jsltkubrick@users.noreply.github.com>
2023-04-05 21:43:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 
 import streamlit
 
-streamlit.title('My Parents new healthy diner') #sets the app's title
+streamlit.title('My Parents new healthy diner') #sets the app's title - committing this change makes the title appear in the streamlit app
