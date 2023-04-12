7eb0c34a421242b894cdd4322d7423f64daf3be4
jsltkubrick <127206945+jsltkubrick@users.noreply.github.com>
2023-04-12 13:00:09 +0100

    Update streamlit_app.py
    added in function that is called as part of try except loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]      #use the fruits in our 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruity_vice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -36,9 +41,8 @@ try:
   if not fruit_choice:
     streamlit.error('please select a fruit to get info!')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
  
 except URLError as e:
   streamlit.error()
